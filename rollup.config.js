146528fa225862e7312a1d37a602a1ddc23d5e17
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-10-30 08:40:33 +0100

    Updated packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,21 @@
-import babel from '@rollup/plugin-babel';
-import external from 'rollup-plugin-peer-deps-external';
-import del from 'rollup-plugin-delete';
-import pkg from './package.json';
+import babel from '@rollup/plugin-babel'
+import external from 'rollup-plugin-peer-deps-external'
+import del from 'rollup-plugin-delete'
+import pkg from './package.json'
 
 export default {
-    input: pkg.source,
-    output: [
-        { file: pkg.main, format: 'cjs' },
-        { file: pkg.module, format: 'esm' }
-    ],
-    plugins: [
-        external(),
-        babel({
-            exclude: 'node_modules/**',
-            babelHelpers: 'bundled' 
-        }),
-        del({ targets: ['dist/*'] }),
-    ],
-    external: Object.keys(pkg.peerDependencies || {}),
-};
+  input: pkg.source,
+  output: [
+    { file: pkg.main, format: 'cjs', exports: 'auto' },
+    { file: pkg.module, format: 'esm', exports: 'auto' }
+  ],
+  plugins: [
+    external(),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled'
+    }),
+    del({ targets: ['dist/*'] })
+  ],
+  external: Object.keys(pkg.peerDependencies || {})
+}
